80999095639f01b9fb3d2db612a38bf9fe5786f0
Ryan Deng <ryan.deng32@gmail.com>
2020-01-18 20:42:02 -0500

    add template files

---
+++ UI/pop_up.js
@@ -0,0 +1,7 @@
+document.getElementById("Scotiabank").onclick = function () {
+    //location.href = "https://www.scotiabank.com/ca/en/personal.html";
+    window.open(
+      'https://www.scotiabank.com/ca/en/personal.html',
+      '_blank'
+    )
+};
