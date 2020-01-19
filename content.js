54bff4be28cf398756663616e9bff86b1daf5c97
William Tran <w25tran@uwaterloo.ca>
2020-01-18 21:59:15 -0500

    willy needs to fix his file system

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
-alert("ShopSmart")
+alert("ShopSmart");
+
+// disable buy now
+document.getElementById("buy-now-button").disabled = true;
+document.getElementById("buy-now-button").style.cursor = "auto";
+document.getElementById("submit.buy-now").getElementsByTagName('span')[0].style.background = "#565656";
+document.getElementById("submit.buy-now").classList.remove("a-button-oneclick");
+document.getElementById("submit.buy-now-announce").innerHTML = 'Buy Now (Disabled)';
